6d6f457d9acae8a9e350b494257f9a75885bcec9
Nguyễn Thanh Nhật <145891189+night0-git@users.noreply.github.com>
2025-04-26 13:11:31 +0700

    Update helper.h

--- include/helper.h
+++ include/helper.h
@@ -2,8 +2,11 @@
 #include <random>
 using std::vector;
 
+// swap two elements
 void swap(int& a, int& b);
 
+// check if array is sorted
 bool isSorted(vector<int> arr);
 
+// generate a random array
 vector<int> arrayInit(int n, int k);
